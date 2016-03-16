fc11fe2a6880cd3bc415fbc1e1f58790e62dbcee
egor-a-p <egor.a.petrov@gmail.com>
2016-03-16 13:26:41 +0300

    Task 1654. Accepted.

--- src/ru/timus/acm/Task_1654.java
+++ src/ru/timus/acm/Task_1654.java
@@ -8,7 +8,16 @@ public class Task_1654 {
     public static void main(String[] args) throws IOException {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         String line = consoleReader.readLine();
-        consoleReader.close();
+        StringBuilder output = new StringBuilder();
 
+        for (char c : line.toCharArray()) {
+            int l = output.length();
+            if (l != 0 && output.charAt(l - 1) == c)
+                output.deleteCharAt(l - 1);
+            else
+                output.append(c);
+        }
+
+        System.out.println(output);
     }
 }
