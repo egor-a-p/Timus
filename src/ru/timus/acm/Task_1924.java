079b11fa75a2a0c4e77cc0e80bc6863e01f0fef5
egor-a-p <egor.a.petrov@gmail.com>
2016-03-17 10:04:23 +0300

    Task 1924. Accepted.

---
+++ src/ru/timus/acm/Task_1924.java
@@ -0,0 +1,20 @@
+package ru.timus.acm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_1924 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(consoleReader.readLine());
+        consoleReader.close();
+
+        if ((n * (n + 1) / 2) % 2 == 0)
+            System.out.println("black");
+        else
+            System.out.println("grimy");
+    }
+}
