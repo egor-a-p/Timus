abd2908b6db1c96394c7ba1249dbc467728fc7f0
egor-a-p <egor.a.petrov@gmail.com>
2016-03-17 11:26:28 +0300

    Task 1493. Accepted.

--- src/ru/timus/acm/Task_1493.java
+++ src/ru/timus/acm/Task_1493.java
@@ -4,9 +4,9 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Task_1493 {
-    private static int getNumberSum(int number) {
-        int hundred = number / 100;
-        return (number - ((hundred << 6) + (hundred << 5) + (hundred << 2))) / 10 + hundred;
+    private static int getSum(int n) {
+        int h = n / 100;
+        return (n - ((h << 6) + (h << 5) + (h << 2))) / 10 + h;
     }
 
     public static void main(String str[]) throws Exception {
@@ -14,25 +14,24 @@ public class Task_1493 {
         String numberLine = consoleReader.readLine();
         consoleReader.close();
 
-        int firNum = Character.getNumericValue(numberLine.charAt(0))
+        int firstNumber = Character.getNumericValue(numberLine.charAt(0))
                 + Character.getNumericValue(numberLine.charAt(1))
                 + Character.getNumericValue(numberLine.charAt(2));
 
-        int secNum = Integer.parseInt(numberLine.substring(3));
+        int secondNumber = Integer.parseInt(numberLine.substring(3));
 
         int last = Character.getNumericValue(numberLine.charAt(5));
-        int numberSumIncr = getNumberSum(secNum + 1);
-        int numberSumDecr = getNumberSum(secNum - 1);
+        int increment = getSum(secondNumber + 1);
+        int decrement = getSum(secondNumber - 1);
         if (last != 9)
-            numberSumIncr += last + 1;
+            increment += last + 1;
         if (last == 0)
-            numberSumDecr += 9;
+            decrement += 9;
         else
-            numberSumDecr += last - 1;
-        if (firNum == numberSumIncr || firNum == numberSumDecr)
+            decrement += last - 1;
+        if (firstNumber == increment || firstNumber == decrement)
             System.out.println("Yes");
         else
             System.out.println("No");
-
     }
 }
