e219a0c33aa7b8d61e387553f1e4e003f0f5d363
egor-a-p <egor.a.petrov@gmail.com>
2016-03-18 17:53:38 +0300

    Task 1517. Restricted function.

--- src/ru/timus/acm/Task_1517.java
+++ src/ru/timus/acm/Task_1517.java
@@ -3,87 +3,92 @@ package ru.timus.acm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashSet;
+import java.util.concurrent.*;
 
 public class Task_1517 {
 
-    static class StringSuffix implements Comparable<StringSuffix>{
-        char start;
-        int indexFrom;
+    private static class Task implements Callable<String>{
+        String line1,line2;
+        int n;
+        int i;
 
-        StringSuffix(String s, int indexFrom) {
-            start = s.charAt(indexFrom);
-            this.indexFrom = indexFrom;
+        public Task(int n, int i, String line1, String line2) {
+            this.line1 = line1;
+            this.line2 = line2;
+            this.n = n;
+            this.i = i;
         }
 
-        public int compareTo(StringSuffix o) {
-            int dc = Character.compare(this.start, o.start);
-            int di = Integer.compare(this.indexFrom, o.indexFrom);
-            if (dc != 0)
-                return dc;
-            else
-                return di;
-        }
-    }
-
-    private static StringSuffix[] getSuffixes(String input){
-        StringSuffix[] suffixes = new StringSuffix[input.length()];
-        for (int i = 0; i < input.length(); i++)
-            suffixes[i] = new StringSuffix(input, i);
-
-        Arrays.parallelSort(suffixes);
-        /*for (StringSuffix s : suffixes)
-            System.out.println(input.substring(s.indexFrom));*/
-        return suffixes;
-    }
-
-    private static boolean contains(StringSuffix[] suffixes, String input, String search){
-        StringSuffix s = new StringSuffix(search, 0);
-        int ip = Arrays.binarySearch(suffixes, s);
-        if (ip < 0)
-            ip = -ip - 1;
-
-        return ip < suffixes.length && input.substring(suffixes[ip].indexFrom).startsWith(search);
-    }
-
-    private static String getSameSubstring(int n, String line1, String line2){
-        StringSuffix[] suffixes = getSuffixes(line2);
-        for (int i = n; i > 0; i--)
-            for (int j = 0; j + i <= n; j++) {
-                String s1 = line1.substring(j, j + i);
-                if (contains(suffixes, line2, s1))
-                    return s1;
+        @Override
+        public String call() throws Exception {
+            HashSet<StringWrapper> set = new HashSet<>(26);
+            for (int j = 0; j + i <= n; j++){
+                StringWrapper sw1 = new StringWrapper(line1.substring(j, j + i), 1);
+                StringWrapper sw2 = new StringWrapper(line2.substring(j, j + i), 2);
+                if (!set.add(sw1))
+                    return sw1.string;
+                else if (!set.add(sw2))
+                    return sw2.string;
             }
+            set.clear();
+            return null;
+        }
+    }
+
+    private static String getSpeech(int n, String line1, String line2) throws ExecutionException, InterruptedException {
+        Future<String>[] futures = new Future[n];
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        for (int i = n; i > 0; i--)
+            futures[n - i] = service.submit(new Task(n, i, line1, line2));
+
+        for (Future<String> future : futures){
+            String result = future.get();
+            if (result != null){
+                service.shutdownNow();
+                return result;
+            }
+        }
 
         return "";
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(consoleReader.readLine());
         String line1 = consoleReader.readLine();
         String line2 = consoleReader.readLine();
-        consoleReader.close();
-        System.out.println(getSameSubstring(n, line1, line2));
+        System.out.println(getSpeech(n, line1, line2));
+    }
 
+    private static class StringWrapper {
+        final String string;
+        final int id;
 
-        /*char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        Random random = new Random();
-        for (int i = 0; i < 5000; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            sb1.append(c);
-            c = chars[random.nextInt(chars.length)];
-            sb2.append(c);
+        public StringWrapper(String string, int id) {
+            this.string = string;
+            this.id = id;
         }
-        String output1 = sb1.toString().toUpperCase();
-        String output2 = sb2.toString().toUpperCase();
-        System.out.println(output1);
-        System.out.println(output2);
 
-        Date start = new Date();
-        System.out.println(getSameSubstring(5000, output1, output2));
-        System.out.println("Time: " + (new Date().getTime() - start.getTime()));*/
+        @Override
+        public String toString() {
+            return string;
+        }
+
+        @Override
+        public int hashCode() {
+            return string.charAt(0);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            StringWrapper s = (StringWrapper) obj;
+
+            if (this.id == s.id)
+                return false;
+
+            return string.equals(s.string);
+        }
     }
 }
