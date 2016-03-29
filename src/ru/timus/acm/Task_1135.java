0319b0ca4a18538c52908b84a807301589e159a6
egor-a-p <egor.a.petrov@gmail.com>
2016-03-29 18:42:46 +0300

    Task 1135. Accepted.

---
+++ src/ru/timus/acm/Task_1135.java
@@ -0,0 +1,30 @@
+package ru.timus.acm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task_1135 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(consoleReader.readLine());
+        int close = 0;
+        int result = 0;
+
+        int i = 0;
+        while (i < n) {
+            char c = (char) consoleReader.read();
+            if (c == '>') {
+                close++;
+                i++;
+            } else if (c == '<') {
+                result += close;
+                i++;
+            }
+        }
+
+
+        System.out.println(result);
+    }
+}
