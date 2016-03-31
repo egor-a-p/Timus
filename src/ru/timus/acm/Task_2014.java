f04821bd2dc11fec6b0cc25101e6d5aa18129950
egor-a-p <egor.a.petrov@gmail.com>
2016-03-31 20:28:18 +0300

    Task 1869. Accepted.

--- src/ru/timus/acm/Task_2014.java
+++ src/ru/timus/acm/Task_2014.java
@@ -7,45 +7,78 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Task_2014 {
-
-    private static TreeMap<Date, Integer> balance = new TreeMap<>();
     private static SimpleDateFormat format = new SimpleDateFormat("dd.MM HH:mm");
-    private static List<Integer> result;
+    private static Deque<Record> ledger = new LinkedList<>();
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(consoleReader.readLine());
-        result = new ArrayList<>(n);
+        List<Integer> credit = new ArrayList<>(n);
 
         for (int i = 0; i < n; i++)
-            credit(consoleReader.readLine().split(" "));
+            credit.add(addRecord(consoleReader.readLine()));
 
-        for (int i = 0; i < n; i++)
-            System.out.println(result.get(i));
+        credit.stream().forEach(integer -> System.out.println(integer));
     }
 
-    private static void credit(String[] input) throws ParseException {
-        int value = Integer.parseInt(input[0]);
-        Date date = format.parse(input[1] + " " + input[2]);
-        balance.put(date, value);
-        int purse = 0;
-        int card = 0;
+    private static int addRecord(String line) throws ParseException {
+        Record currentRecord = new Record(line);
 
-        for (Map.Entry<Date, Integer> e : balance.entrySet()) {
-            int op = e.getValue();
-            if (op >= 0)
-                purse += op;
-            else {
-                if (Math.abs(op) <= purse)
-                    purse += op;
-                else {
-                    card += purse + op;
-                    purse = 0;
+        if (ledger.isEmpty()) {
+            currentRecord.setPurseAndCard(0, 0);
+            ledger.offerLast(currentRecord);
+            return currentRecord.card;
+        } else {
+            Record previousRecord = ledger.peekLast();
+            if (previousRecord.date.compareTo(currentRecord.date) < 0) {
+                currentRecord.setPurseAndCard(previousRecord.purse, previousRecord.card);
+                ledger.offerLast(currentRecord);
+            } else {
+                Deque<Record> temp = new LinkedList<>();
+
+                while (!ledger.isEmpty() && ledger.peekLast().date.compareTo(currentRecord.date) > 0)
+                    temp.offerLast(ledger.pollLast());
+
+                if (!ledger.isEmpty()) {
+                    previousRecord = ledger.peekLast();
+                    currentRecord.setPurseAndCard(previousRecord.purse, previousRecord.card);
+                } else
+                    currentRecord.setPurseAndCard(0, 0);
+
+                ledger.offerLast(currentRecord);
+
+                while (!temp.isEmpty()) {
+                    currentRecord = temp.pollLast();
+                    previousRecord = ledger.peekLast();
+                    currentRecord.setPurseAndCard(previousRecord.purse, previousRecord.card);
+                    ledger.offerLast(currentRecord);
                 }
             }
+
+            return ledger.peekLast().card;
+        }
+    }
+
+    private static class Record {
+        int amount, purse, card;
+        Date date;
+
+        public Record(String line) throws ParseException {
+            String[] input = line.split(" ");
+            amount = Integer.parseInt(input[0]);
+            this.date = format.parse(input[1] + " " + input[2]);
         }
 
-        result.add(card);
+        public void setPurseAndCard(int previousPurse, int previousCard) {
+            if (amount >= 0 || Math.abs(amount) <= previousPurse) {
+                purse = previousPurse + amount;
+                card = previousCard;
+            } else {
+                purse = 0;
+                card = previousCard + previousPurse + amount;
+            }
+        }
     }
 
 }
