408eea912f8ab3d1f741a73eb8d9c56937c4ea97
egor-a-p <egor.a.petrov@gmail.com>
2016-04-09 19:43:58 +0300

    Task 1517. Memory limit exceed.

--- src/ru/timus/acm/Task_1517.java
+++ src/ru/timus/acm/Task_1517.java
@@ -3,109 +3,39 @@ package ru.timus.acm;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.security.SecureRandom;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.*;
 
 public class Task_1517 {
 
-    private static String getSpeech(int n, String line1, String line2) throws ExecutionException, InterruptedException {
-        List<Future<String>> futures = new LinkedList<>();
-        ExecutorService service = Executors.newCachedThreadPool();
-
-        for (int i = n; i > 0; i--)
-            futures.add(service.submit(new Task(n, i, line1, line2)));
-
-        for (Future<String> future : futures) {
-            String result = future.get();
-            if (result != null) {
-                service.shutdownNow();
-                return result;
-            }
-        }
-
-        return "";
-    }
-
-    private static String randomString(int len) {
-        final String AB = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        SecureRandom rnd = new SecureRandom();
-        StringBuilder sb = new StringBuilder(len);
-        for (int i = 0; i < len; i++)
-            sb.append(AB.charAt(rnd.nextInt(AB.length())));
-        return sb.toString();
-    }
-
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
-        /*int n = Integer.parseInt(consoleReader.readLine());
+        int n = Integer.parseInt(consoleReader.readLine());
         String line1 = consoleReader.readLine();
-        String line2 = consoleReader.readLine();*/
-        int n = 5000;
-        String line1 = randomString(n);
-        String line2 = randomString(n);
-        System.out.println(getSpeech(n, line1, line2));
+        String line2 = consoleReader.readLine();
+        System.out.println(longestCS(n, line1, line2));
     }
 
-    private static class Task implements Callable<String> {
-        String line1, line2;
-        int n;
-        int i;
 
-        public Task(int n, int i, String line1, String line2) {
-            this.line1 = line1;
-            this.line2 = line2;
-            this.n = n;
-            this.i = i;
-        }
+    private static String longestCS(int n, String a, String b) {
+        if (a.equals(b)) return a;
 
-        @Override
-        public String call() throws Exception {
-            HashSet<StringWrapper> set = new HashSet<>(26);
-            for (int j = 0; j + i <= n; j++) {
-                StringWrapper sw1 = new StringWrapper(line1.substring(j, j + i), 1);
-                StringWrapper sw2 = new StringWrapper(line2.substring(j, j + i), 2);
-                if (!set.add(sw1))
-                    return sw1.string;
-                else if (!set.add(sw2))
-                    return sw2.string;
-            }
-            set.clear();
-            return null;
-        }
-    }
+        int[][] matrix = new int[n][n];
 
-    private static class StringWrapper {
-        final String string;
-        final int id;
+        int maxLength = 0;
+        int maxI = 0;
 
-        public StringWrapper(String string, int id) {
-            this.string = string;
-            this.id = id;
-        }
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (a.charAt(i) == b.charAt(j)) {
+                    if (i != 0 && j != 0)
+                        matrix[i][j] = matrix[i - 1][j - 1] + 1;
+                    else
+                        matrix[i][j] = 1;
+                    if (matrix[i][j] > maxLength) {
+                        maxLength = matrix[i][j];
+                        maxI = i;
+                    }
+                }
 
-        @Override
-        public String toString() {
-            return string;
-        }
-
-        @Override
-        public int hashCode() {
-            return string.charAt(0);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-
-            if (obj instanceof StringWrapper) {
-                StringWrapper s = (StringWrapper) obj;
-                return this.id != s.id && string.equals(s.string);
-            }
-            return false;
-        }
+        return a.substring(maxI - maxLength + 1, maxI + 1);
     }
 }
